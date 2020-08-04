93e8cce4ac0919c5271c4ce24bd428dc487023d9
chuhx <hx.chu@clustar.ai>
2020-08-04 15:25:16 +0800

    feat: 按需引入 ele-ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import { Button } from 'element-ui'
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
